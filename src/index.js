34043f8b36131a38b94229da528ce43e46103149
pzdesu <pakerchang@gmail.com>
2023-05-07 15:49:23 +0800

    :tada: Project init.

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import fs from "fs";
+import path from "path";
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const executeRoot = process.cwd();
+
+function findNodeModulesDirectories(directoryPath, results) {
+  const files = fs.readdirSync(directoryPath);
+
+  for (const file of files) {
+    const filePath = path.join(directoryPath, file);
+    const isDirectory = fs.statSync(filePath).isDirectory();
+
+    if (isDirectory && file === "node_modules") {
+      results.push(filePath);
+    } else if (isDirectory) {
+      findNodeModulesDirectories(filePath, results);
+    }
+  }
+}
+
+function deleteTarget(filePaths) {
+  for (const filePath of filePaths) {
+    fs.rmdirSync(
+      filePath,
+      { recursive: true },
+      (err) => err && console.log(chalk.redBright(`Error: ${err}`))
+    );
+    console.log(chalk.greenBright(`Deleted `), chalk.yellowBright(filePath));
+  }
+}
+
+function main() {
+  const nodeModulePaths = [];
+  findNodeModulesDirectories(executeRoot, nodeModulePaths);
+
+  if (nodeModulePaths.length === 0)
+    return console.log(chalk.yellowBright(" node_modules is not found"));
+
+  console.log(chalk.yellowBright("Paths: "));
+  for (const nodeModulePath of nodeModulePaths) {
+    console.log(chalk.cyanBright(nodeModulePath));
+  }
+
+  inquirer
+    .prompt({
+      type: "confirm",
+      name: "Confirm",
+      message: "Delete all node_modules folder? ",
+    })
+    .then((confirm) => {
+      if (!confirm) {
+        return;
+      } else {
+        deleteTarget(nodeModulePaths);
+        return console.log(chalk.green("Compelete!"));
+      }
+    });
+}
+
+export default main();
